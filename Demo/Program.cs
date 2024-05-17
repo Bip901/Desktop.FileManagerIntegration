796d628ee9d03c0dd8f0ce515c9b3d6576ad1d8a
Bip901 <74911767+Bip901@users.noreply.github.com>
2024-05-17 12:43:20 +0300

    Add demo project

---
+++ Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using Desktop.FileManagerIntegration;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? exampleFile = Directory.EnumerateFiles(AppContext.BaseDirectory).FirstOrDefault() ?? Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            FileManager.HighlightInContainingFolder(exampleFile);
+        }
+    }
+}
